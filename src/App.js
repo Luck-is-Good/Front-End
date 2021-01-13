5fbec26b99789d6b7cf198e3f77e61b300567c67
songhyunjung <sss3593@naver.com>
2021-01-13 22:44:45 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
+
 const containerStyle = {
   width: '1600px',
   height: '900px'
@@ -17,8 +18,51 @@ const icons = {
   },
 };
 
+function TextBox() {
+
+  return (
+      <div>
+        <h1>Your current location</h1>
+        <button>YES</button>
+        <p></p>
+        <button>NO</button>
+      </div>
+  );
+  
+}
+
+function ButtonBox(){
+  return (
+      <div >
+        <button> <img src="/home.png" alt="home" width="50px" height="50px"/></button>
+        <button> <img src="/user.png" alt="user" width="50px" height="50px"/></button>
+        <button> <img src="/add.png" alt="add" width="50px" height="50px"/></button>
+        <button> <img src="/settings.png" alt="setting" width="50px" height="50px"/></button>
+      </div>
+  );
+  
+
+}
+
+function InputBox() {
+  return (
+    <form >
+      name <input type="text" name="name" />
+      <p></p>
+      age <input type="text" name="age" />
+      <p></p>
+      Maximum allowable distance <input type="text" name="distance" />
+      <p></p>
+      <button>save</button>
+    </form>
+);
+}
+
+
+
 function MyComponent() {
   return (
+      
       <LoadScript
         googleMapsApiKey="AIzaSyBOluu7JbYoawEF0aMk4cgsPk_Fbx5GrGs"
       >
@@ -34,7 +78,22 @@ function MyComponent() {
           />
         </GoogleMap>
       </LoadScript>
+      
    )
  }
 
-export default React.memo(MyComponent)
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <ButtonBox />
+      <MyComponent />
+      <TextBox />
+      <InputBox />
+      </div>
+    );
+  }
+  
+}
+
+export default React.memo(App) 
